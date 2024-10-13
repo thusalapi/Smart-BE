a05024c3c97ef07d4272c125c9a53599078a7b42
Thusala Piyarisi <thusalapi@gmail.com>
2024-10-13 21:26:07 +0530

    first commit

---
+++ src/main/java/org/trash/smartbe/SmartBeApplication.java
@@ -0,0 +1,13 @@
+package org.trash.smartbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/trash/smartbe/SmartBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trash.smartbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
